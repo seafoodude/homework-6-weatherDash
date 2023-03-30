072e7cd37c60075fa84d5867fb93a8410d1b2c47
seafoodude <michaelhuang777@gmail.com>
2023-03-29 19:03:14 -0700

    did media query and named my variables

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,6 @@
-var client_id = window.event.API_KEY;
+var key = window.event.API_KEY;
 
+var search = document.querySelector(".input");
+var button = document.querySelector("#search");
+var clear = document.querySelector("#clear");
+var today = document.querySelector(".today");
